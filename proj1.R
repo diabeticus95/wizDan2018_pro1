f8ff32169bb25be4703a87be66b83bbc57568ded
diabeticus95 <diabeticus95@gmail.com>
2019-03-29 20:56:05 +0100

    wordcloud

--- proj1.R
+++ proj1.R
@@ -34,7 +34,7 @@ messg <- lapply(1:length(messg), function(i) {
 }
 )
 
-#1. najczesciej uzywane slowa
+######1. najczesciej uzywane slowa#####
 stopw <- fromJSON(file = "D:\\mgr\\dyplom\\R_analysis\\stopwords.txt")
 unl.times <- unlist(times, recursive = T) 
 unl.sender <- unlist(sender, recursive = F) #sendery też są nullami niektóre
@@ -53,9 +53,157 @@ df.words <- df %>%
 top <- df.words %>%
   count(word, sort = TRUE) 
 
-head(top,20)
+#ręcznie wybrałem top 20 sensownych słów -> subiektywne określenie stopwordów, wywalenie emot, wulgaryzmów.
+top_sel <- read.table(header = F, file = "D:\\mgr\\wizualizacja_danych\\proj1\\top_words.txt")
+top_sel <- top[top_sel$V1,]
 
-#2. zbinować to i nakreślić w funkcji timestamp
+#wordcloud
+
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+wordcloud(words = top_sel$word, freq = top_sel$n,  min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+##### wulgaryzmy #####
+##### import #####
+wulg.pl <- c('chuj','chuja', 'chujek', 'chuju', 'chujem', 'chujnia',
+              'chujowy', 'chujowa', 'chujowe', 'cipa', 'cipę', 'cipe', 'cipą',
+              'cipie', 'dojebać','dojebac', 'dojebie', 'dojebał', 'dojebal',
+              'dojebała', 'dojebala', 'dojebałem', 'dojebalem', 'dojebałam',
+              'dojebalam', 'dojebię', 'dojebie', 'dopieprzać', 'dopieprzac',
+              'dopierdalać', 'dopierdalac', 'dopierdala', 'dopierdalał',
+              'dopierdalal', 'dopierdalała', 'dopierdalala', 'dopierdoli',
+              'dopierdolił', 'dopierdolil', 'dopierdolę', 'dopierdole', 'dopierdoli',
+              'dopierdalający', 'dopierdalajacy', 'dopierdolić', 'dopierdolic',
+              'dupa', 'dupie', 'dupą', 'dupcia', 'dupeczka', 'dupy', 'dupe', 'huj',
+              'hujek', 'hujnia', 'huja', 'huje', 'hujem', 'huju', 'jebać', 'jebac',
+              'jebał', 'jebal', 'jebie', 'jebią', 'jebia', 'jebak', 'jebaka', 'jebal',
+              'jebał', 'jebany', 'jebane', 'jebanka', 'jebanko', 'jebankiem',
+              'jebanymi', 'jebana', 'jebanym', 'jebanej', 'jebaną', 'jebana',
+              'jebani', 'jebanych', 'jebanymi', 'jebcie', 'jebiący', 'jebiacy',
+              'jebiąca', 'jebiaca', 'jebiącego', 'jebiacego', 'jebiącej', 'jebiacej',
+              'jebia', 'jebią', 'jebie', 'jebię', 'jebliwy', 'jebnąć', 'jebnac',
+              'jebnąc', 'jebnać', 'jebnął', 'jebnal', 'jebną', 'jebna', 'jebnęła',
+              'jebnela', 'jebnie', 'jebnij', 'jebut', 'koorwa', 'kórwa', 'kurestwo',
+              'kurew', 'kurewski', 'kurewska', 'kurewskiej', 'kurewską', 'kurewska',
+              'kurewsko', 'kurewstwo', 'kurwa', 'kurwaa', 'kurwami', 'kurwą', 'kurwe',
+              'kurwę', 'kurwie', 'kurwiska', 'kurwo', 'kurwy', 'kurwach', 'kurwami',
+              'kurewski', 'kurwiarz', 'kurwiący', 'kurwica', 'kurwić', 'kurwic',
+              'kurwidołek', 'kurwik', 'kurwiki', 'kurwiszcze', 'kurwiszon',
+              'kurwiszona', 'kurwiszonem', 'kurwiszony', 'kutas', 'kutasa', 'kutasie',
+              'kutasem', 'kutasy', 'kutasów', 'kutasow', 'kutasach', 'kutasami',
+              'matkojebca', 'matkojebcy', 'matkojebcą', 'matkojebca', 'matkojebcami',
+              'matkojebcach', 'nabarłożyć', 'najebać', 'najebac', 'najebał',
+              'najebal', 'najebała', 'najebala', 'najebane', 'najebany', 'najebaną',
+              'najebana', 'najebie', 'najebią', 'najebia', 'naopierdalać',
+              'naopierdalac', 'naopierdalał', 'naopierdalal', 'naopierdalała',
+              'naopierdalala', 'naopierdalała', 'napierdalać', 'napierdalac',
+              'napierdalający', 'napierdalajacy', 'napierdolić', 'napierdolic',
+              'nawpierdalać', 'nawpierdalac', 'nawpierdalał', 'nawpierdalal',
+              'nawpierdalała', 'nawpierdalala', 'obsrywać', 'obsrywac', 'obsrywający',
+              'obsrywajacy', 'odpieprzać', 'odpieprzac', 'odpieprzy', 'odpieprzył',
+              'odpieprzyl', 'odpieprzyła', 'odpieprzyla', 'odpierdalać',
+              'odpierdalac', 'odpierdol', 'odpierdolił', 'odpierdolil',
+              'odpierdoliła', 'odpierdolila', 'odpierdoli', 'odpierdalający',
+              'odpierdalajacy', 'odpierdalająca', 'odpierdalajaca', 'odpierdolić',
+              'odpierdolic', 'odpierdoli', 'odpierdolił', 'opieprzający',
+              'opierdalać', 'opierdalac', 'opierdala', 'opierdalający',
+              'opierdalajacy', 'opierdol', 'opierdolić', 'opierdolic', 'opierdoli',
+              'opierdolą', 'opierdola', 'piczka', 'pieprznięty', 'pieprzniety',
+              'pieprzony', 'pierdel', 'pierdlu', 'pierdolą', 'pierdola', 'pierdolący',
+              'pierdolacy', 'pierdoląca', 'pierdolaca', 'pierdol', 'pierdole',
+              'pierdolenie', 'pierdoleniem', 'pierdoleniu', 'pierdolę', 'pierdolec',
+              'pierdola', 'pierdolą', 'pierdolić', 'pierdolicie', 'pierdolic',
+              'pierdolił', 'pierdolil', 'pierdoliła', 'pierdolila', 'pierdoli',
+              'pierdolnięty', 'pierdolniety', 'pierdolisz', 'pierdolnąć',
+              'pierdolnac', 'pierdolnął', 'pierdolnal', 'pierdolnęła', 'pierdolnela',
+              'pierdolnie', 'pierdolnięty', 'pierdolnij', 'pierdolnik', 'pierdolona',
+              'pierdolone', 'pierdolony', 'pierdołki', 'pierdzący', 'pierdzieć',
+              'pierdziec', 'pizda', 'pizdą', 'pizde', 'pizdę', 'piździe', 'pizdzie',
+              'pizdnąć', 'pizdnac', 'pizdu', 'podpierdalać', 'podpierdalac',
+              'podpierdala', 'podpierdalający', 'podpierdalajacy', 'podpierdolić',
+              'podpierdolic', 'podpierdoli', 'pojeb', 'pojeba', 'pojebami',
+              'pojebani', 'pojebanego', 'pojebanemu', 'pojebani', 'pojebany',
+              'pojebanych', 'pojebanym', 'pojebanymi', 'pojebem', 'pojebać',
+              'pojebac', 'pojebalo', 'popierdala', 'popierdalac', 'popierdalać',
+              'popierdolić', 'popierdolic', 'popierdoli', 'popierdolonego',
+              'popierdolonemu', 'popierdolonym', 'popierdolone', 'popierdoleni',
+              'popierdolony', 'porozpierdalać', 'porozpierdala', 'porozpierdalac',
+              'poruchac', 'poruchać', 'przejebać', 'przejebane', 'przejebac',
+              'przyjebali', 'przepierdalać', 'przepierdalac', 'przepierdala',
+              'przepierdalający', 'przepierdalajacy', 'przepierdalająca',
+              'przepierdalajaca', 'przepierdolić', 'przepierdolic', 'przyjebać',
+              'przyjebac', 'przyjebie', 'przyjebała', 'przyjebala', 'przyjebał',
+              'przyjebal', 'przypieprzać', 'przypieprzac', 'przypieprzający',
+              'przypieprzajacy', 'przypieprzająca', 'przypieprzajaca',
+              'przypierdalać', 'przypierdalac', 'przypierdala', 'przypierdoli',
+              'przypierdalający', 'przypierdalajacy', 'przypierdolić',
+              'przypierdolic', 'qrwa', 'rozjebać', 'rozjebac', 'rozjebie',
+              'rozjebała', 'rozjebią', 'rozpierdalać', 'rozpierdalac', 'rozpierdala',
+              'rozpierdolić', 'rozpierdolic', 'rozpierdole', 'rozpierdoli',
+              'rozpierducha', 'skurwić', 'skurwiel', 'skurwiela', 'skurwielem',
+              'skurwielu', 'skurwysyn', 'skurwysynów', 'skurwysynow', 'skurwysyna',
+              'skurwysynem', 'skurwysynu', 'skurwysyny', 'skurwysyński',
+              'skurwysynski', 'skurwysyństwo', 'skurwysynstwo', 'spieprzać',
+              'spieprzac', 'spieprza', 'spieprzaj', 'spieprzajcie', 'spieprzają',
+              'spieprzaja', 'spieprzający', 'spieprzajacy', 'spieprzająca',
+              'spieprzajaca', 'spierdalać', 'spierdalac', 'spierdala', 'spierdalał',
+              'spierdalała', 'spierdalal', 'spierdalalcie', 'spierdalala',
+              'spierdalający', 'spierdalajacy', 'spierdolić', 'spierdolic',
+              'spierdoli', 'spierdoliła', 'spierdoliło', 'spierdolą', 'spierdola',
+              'srać', 'srac', 'srający', 'srajacy', 'srając', 'srajac', 'sraj',
+              'sukinsyn', 'sukinsyny', 'sukinsynom', 'sukinsynowi', 'sukinsynów',
+              'sukinsynow', 'śmierdziel', 'udupić', 'ujebać', 'ujebac', 'ujebał',
+              'ujebal', 'ujebana', 'ujebany', 'ujebie', 'ujebała', 'ujebala',
+              'upierdalać', 'upierdalac', 'upierdala', 'upierdoli', 'upierdolić',
+              'upierdolic', 'upierdoli', 'upierdolą', 'upierdola', 'upierdoleni',
+              'wjebać', 'wjebac', 'wjebie', 'wjebią', 'wjebia', 'wjebiemy',
+              'wjebiecie', 'wkurwiać', 'wkurwiac', 'wkurwi', 'wkurwia', 'wkurwiał',
+              'wkurwial', 'wkurwiający', 'wkurwiajacy', 'wkurwiająca', 'wkurwiajaca',
+              'wkurwić', 'wkurwic', 'wkurwi', 'wkurwiacie', 'wkurwiają', 'wkurwiali',
+              'wkurwią', 'wkurwia', 'wkurwimy', 'wkurwicie', 'wkurwiacie', 'wkurwić',
+              'wkurwic', 'wkurwia', 'wpierdalać', 'wpierdalac', 'wpierdalający',
+              'wpierdalajacy', 'wpierdol', 'wpierdolić', 'wpierdolic', 'wpizdu',
+              'wyjebać', 'wyjebac', 'wyjebali', 'wyjebał', 'wyjebac', 'wyjebała',
+              'wyjebały', 'wyjebie', 'wyjebią', 'wyjebia', 'wyjebiesz', 'wyjebie',
+              'wyjebiecie', 'wyjebiemy', 'wypieprzać', 'wypieprzac', 'wypieprza',
+              'wypieprzał', 'wypieprzal', 'wypieprzała', 'wypieprzala', 'wypieprzy',
+              'wypieprzyła', 'wypieprzyla', 'wypieprzył', 'wypieprzyl', 'wypierdal',
+              'wypierdalać', 'wypierdalac', 'wypierdala', 'wypierdalaj',
+              'wypierdalał', 'wypierdalal', 'wypierdalała', 'wypierdalala',
+              'wypierdalać', 'wypierdolić', 'wypierdolic', 'wypierdoli',
+              'wypierdolimy', 'wypierdolicie', 'wypierdolą', 'wypierdola',
+              'wypierdolili', 'wypierdolił', 'wypierdolil', 'wypierdoliła',
+              'wypierdolila', 'zajebać', 'zajebac', 'zajebie', 'zajebią', 'zajebia',
+              'zajebiał', 'zajebial', 'zajebała', 'zajebiala', 'zajebali', 'zajebana',
+              'zajebani', 'zajebane', 'zajebany', 'zajebanych', 'zajebanym',
+              'zajebanymi', 'zajebiste', 'zajebisty', 'zajebistych', 'zajebista',
+              'zajebistym', 'zajebistymi', 'zajebiście', 'zajebiscie', 'zapieprzyć',
+              'zapieprzyc', 'zapieprzy', 'zapieprzył', 'zapieprzyl', 'zapieprzyła',
+              'zapieprzyla', 'zapieprzą', 'zapieprza', 'zapieprzy', 'zapieprzymy',
+              'zapieprzycie', 'zapieprzysz', 'zapierdala', 'zapierdalać',
+              'zapierdalac', 'zapierdalaja', 'zapierdalał', 'zapierdalaj',
+              'zapierdalajcie', 'zapierdalała', 'zapierdalala', 'zapierdalali',
+              'zapierdalający', 'zapierdalajacy', 'zapierdolić', 'zapierdolic',
+              'zapierdoli', 'zapierdolił', 'zapierdolil', 'zapierdoliła',
+              'zapierdolila', 'zapierdolą', 'zapierdola', 'zapierniczać',
+              'zapierniczający', 'zasrać', 'zasranym', 'zasrywać', 'zasrywający',
+              'zesrywać', 'zesrywający', 'zjebać', 'zjebac', 'zjebał', 'zjebal',
+              'zjebała', 'zjebala', 'zjebana', 'zjebią', 'zjebali', 'zjeby')
+##### ang
+wulg.en <- read.csv('https://raw.githubusercontent.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en')
+wulg <- c(as.character(wulg.en$X2g1c), wulg.pl)
+  
+ile.wulg <- sapply(df$text, function(i){
+    wynik <- sum(str_detect(i, wulg))
+    return(ifelse(wynik > 0, 1, 0))
+  })
+df.wulg <- df %>% 
+
+#####2. zbinować to i nakreślić w funkcji timestamp#####
 #nie chce mi sie zastanawiac jak przepisac timestamp na unnest_tokens, więc tu lecimy calymi zdaniami
 library(scales)
 library(ggplot2)
@@ -72,7 +220,7 @@ bin.df %>%
 #przy średnich 200 wiadomościach na dzień
 mean(bin.df$count)/31
 
-#binowanie po dniu, ciekawe czy mam jakąś serię 0 wiadomosci
+#####binowanie po dniu, ciekawe czy mam jakąś serię 0 wiadomosci#####
 g2 <- ggplot(df, aes(x = times)) +
   scale_x_date(labels = date_format("%Y"), breaks = "1 year") +
   geom_histogram(binwidth = 1) 
